eca29f2220a99d15dc2d12b829bab3091626d33b
BHARATCOMFORT2 <shrrajbhar12340@gmail.com>
2025-08-30 20:30:15 +0530

    Update recommendations.js

--- js/recommendations.js
+++ js/recommendations.js
@@ -1,1 +1,5 @@
+// js/recommendations.js - placeholder to be upgraded with AI
+export async function loadRecommendations(userId, containerId='recommendationContainer'){
+  document.getElementById(containerId).innerHTML = '<p>Recommendations powered by AI will appear here.</p>';
+}
 
